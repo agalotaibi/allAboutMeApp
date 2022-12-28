317c2fd73db2529fae9a42599c165bb7be2ce250
agalotaibi <hopes.gmo@gmail.com>
2022-12-29 00:00:34 +0300

    Initial Commit

---
+++ allAboutMe/allAboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  allAboutMeApp.swift
+//  allAboutMe
+//
+//  Created by HOPES on 05/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct allAboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

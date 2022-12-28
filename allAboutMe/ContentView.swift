0bb90ba134a4f9e9481a88b83c6a7c9f7edf7be0
agalotaibi <hopes.gmo@gmail.com>
2022-12-29 00:06:30 +0300

    short info About me

--- allAboutMe/ContentView.swift
+++ allAboutMe/ContentView.swift
@@ -1,19 +1,20 @@
-//
-//  ContentView.swift
-//  allAboutMe
-//
-//  Created by HOPES on 05/06/1444 AH.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 32) {
+            Image("about").resizable()
+                .frame(width: 320.0, height: 320.0).clipShape(Circle())
+                .shadow(radius: 3)
+                .overlay(Circle().stroke(Color.purple, lineWidth: 7))
+            
+            
+            Text("Amaal AlOtaibi")
+                .font(.largeTitle)
+                .fontWeight(.medium)
+                .foregroundColor(Color.purple)
+            
+            Text("Hello my name is Amaal I'm Majoring in Information Technology and Chemistry, currently I'm a learner at Apple Developer Academy at Saudi Arabia.")
         }
         .padding()
     }
